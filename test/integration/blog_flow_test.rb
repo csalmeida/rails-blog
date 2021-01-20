49069e8d63abe789acf3c804602c332fc37fb36a
cristiano <cs.almeida@protonmail.ch>
2021-01-20 18:44:03 +0000

    Integration test One if articles page shows and another to create an article

---
+++ test/integration/blog_flow_test.rb
@@ -0,0 +1,27 @@
+require "test_helper"
+
+class BlogFlowTest < ActionDispatch::IntegrationTest
+  # An integration test, asserts something the user can do in the application.
+  test "can see articles page" do
+    get "/"
+    assert_select "h1", "Articles"
+  end
+
+  test "can create an article" do
+    get "/articles/new"
+    assert_response :success
+
+    post "/articles", params: {
+      article: {
+        title: "can create",
+        body: "article successfully.",
+        status: "private"
+      }
+    }
+
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_select "h1", "can create"
+  end
+end
