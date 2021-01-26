e244b98aa74b591bfaf5bb84b27ccafc8b82931c
cristiano <cs.almeida@protonmail.ch>
2021-01-26 12:04:46 +0000

    Defines fixture for all tests at once.

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,6 +1,17 @@
 require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
+  # called before every single test
+  setup do
+    @article = articles(:valid)
+  end
+
+  # called after every single test
+  teardown do
+    # when controller is using cache it may be a good idea to reset it afterwards
+    Rails.cache.clear
+  end
+
   test "should get index" do
     get articles_url
     assert_response :success
@@ -11,20 +22,17 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show article" do
-    article = articles(:valid)
-    get article_url(article)
+    get article_url(@article)
     assert_response :success
   end
 
   test "should create article" do
-    article = articles(:valid)
-
     assert_difference("Article.count") do
       post articles_url, params: {
         article: {
-          title: article.title,
-          body: article.body,
-          status: article.status
+          title: @article.title,
+          body: @article.body,
+          status: @article.status
         }
       }
     end
@@ -34,27 +42,25 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   end
   
   test "should destroy article" do
-    article = articles(:valid)
-
     assert_difference("Article.count", -1) do
-      delete article_url(article)
+      delete article_url(@article)
     end
 
     assert_redirected_to articles_path
   end
 
   test "should update article" do
-    article = articles(:valid)
-    patch article_url(article), params: {
+    patch article_url(@article),
+    params: {
       article: {
         title: "Updated title"
       }
     }
 
-    assert_redirected_to article_path(article)
+    assert_redirected_to article_path(@article)
 
     # Reload association to fetch updated data and assert that title is updated.
-    article.reload
-    assert_equal "Updated title", article.title
+    @article.reload
+    assert_equal "Updated title", @article.title
   end
 end
