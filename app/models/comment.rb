d2d7c63dbcdc3c9d341b26fcbee3b07ebb47d3b9
cristiano <cs.almeida@protonmail.ch>
2021-01-26 14:05:35 +0000

    Moves belongs to statement in the file.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,8 @@
 class Comment < ApplicationRecord
   include Visible
+  belongs_to :article
 
   validates :commenter, presence: true
   validates :status, presence: true
   validates :body, presence: true, length: { minimum: 10 }
-
-  belongs_to :article
 end
