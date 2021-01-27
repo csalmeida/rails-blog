2e7932744f2ae8f497dd2aadfe70706e11b0c121
cristiano <cs.almeida@protonmail.ch>
2021-01-27 11:49:52 +0000

    Extended error render helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,19 +3,31 @@ module ApplicationHelper
   # Renders all errors tied to a specific to a field.
   # Useful to show an error message next to the field that triggered it.
   # Might be better to move html to its own partial.
-  # @param field_name#symbol 
-  # @param errors#array
+  # @param field_name#symbol or string if errors in a hash
+  # @param errors#ActiveModel::Errors or hash
   def render_field_errors(field_name, errors=nil)
-    if errors && errors[field_name].size > 0
-      messages = ""
+    messages = ""
 
-      errors.full_messages_for(field_name).each do |error|
-        messages += <<-HTML
-          <p><span style="color:red;">#{error}</span></p>
-        HTML
+    if errors.class == "ActiveModel::Errors"
+      if errors && errors[field_name].size > 0
+
+        errors.full_messages_for(field_name).each do |error|
+          messages += <<-HTML
+            <p><span style="color:red;">#{error}</span></p>
+          HTML
+        end
       end
+    else
+      if errors && errors.size > 0
 
-      return messages.html_safe
+        errors[field_name].each do |error|
+          messages += <<-HTML
+            <p><span style="color:red;">#{error}</span></p>
+          HTML
+        end
+      end
     end
+
+    return messages.html_safe
   end
 end
