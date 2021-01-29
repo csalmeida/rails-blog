917131c5ad3636dec12d68f7b796e6c2eda1e721
cristiano <cs.almeida@protonmail.ch>
2021-01-29 18:41:33 +0000

    Adds helper method to sign user in.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,13 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def sign_in_as(user, password)
+    # This took me a while to achieve. Here's what I missed:
+    # The route needed to be plural as in "sessions" and not "session"
+    # The credentials do not need to be wrapped in a sessions hash like
+    # in other cases because of how the form is structured
+    post sessions_url, params: { email: user.email, password: password }
+  end
+
 end
+
