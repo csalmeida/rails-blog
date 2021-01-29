a6fa2503c71ce3e813ef38427ceb6df46dd4a845
cristiano <cs.almeida@protonmail.ch>
2021-01-29 19:58:03 +0000

    Signs user in when creating articles

--- test/integration/blog_flow_test.rb
+++ test/integration/blog_flow_test.rb
@@ -1,13 +1,17 @@
 require "test_helper"
 
 class BlogFlowTest < ActionDispatch::IntegrationTest
+
   # An integration test, asserts something the user can do in the application.
   test "can see articles page" do
+    sign_out()
     get "/"
     assert_select "h1", "Articles"
   end
 
   test "can create an article" do
+    sign_in_as(users(:valid), "secret")
+    
     get "/articles/new"
     assert_response :success
 
