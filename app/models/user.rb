6afcefee6f27274f18d0fe28c7723a9f382d1a89
cristiano <cs.almeida@protonmail.ch>
2021-02-01 08:54:35 +0000

    Delete articles when a user is deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :articles
+  has_many :articles, dependent: :destroy
   # has_many :comments, through: :articles
 
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
-  validates :user_id, presence: true
 end
